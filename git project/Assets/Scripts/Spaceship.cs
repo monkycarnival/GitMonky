e643118ebf192b6a32a8e24931ebc6c0c5379c0b
DA\nwuser <goroumakisimamu810@gmail.com>
2017-03-13 14:28:17 +0900

    player ・移動する ・画面外に出ない ・弾を撃つ

---
+++ git project/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Spaceship : MonoBehaviour
+{
+    // 移動スピード
+    public float speed;
+
+    // 弾を撃つ間隔
+    public float shotDelay;
+
+    // 弾のPrefab
+    public GameObject bullet;
+
+    // プレイヤーのPrefab
+    public Transform player;
+
+    // 弾を撃つかどうか
+    public int canShot = 0; //0で撃たない 1で撃つ
+
+    //敵が弾を撃ってくる感覚
+    public int time = 0;
+
+    // 爆発のPrefab
+    public GameObject explosion;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // 爆発の作成
+    public void Explosion()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+
+}
